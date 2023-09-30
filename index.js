bbf97ec55b2039a9915575c2865ed9b8a024240d
Sude Ölmez <98457237+sudelmez@users.noreply.github.com>
2023-09-30 23:21:27 +0300

    cors added

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const admin = require('firebase-admin');
+const cors = require('cors');
 
 const app = express();
 const port = 3000;
+app.use(cors());
+
+// const corsOptions = {
+//     origin: 'http://example.com', // İzin vermek istediğiniz kök etki alanı
+//     methods: 'GET,POST', // İzin vermek istediğiniz HTTP yöntemleri
+// };
+
+// app.use(cors(corsOptions));
+// Bu şekilde sadece belirli bir kök etki alanından gelen GET ve POST isteklerine izin verilir.
+// Headers ve Diğer Ayarlar: CORS ayarlarına ek olarak, bazen belirli başlıkların veya diğer ayarların kontrolünü de ele almanız gerekebilir. Bu ayarlar, sunucunuzun isteklere nasıl yanıt vereceğini belirler. Express ile bu ayarları middleware ve yönlendirme işlevleri aracılığıyla yapılandırabilirsiniz.
 
 // Body-parser ayarları
 app.use(bodyParser.json());
